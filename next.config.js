b0916c7007b5f031c166b92fc2687fc41ff06865
laoxiao789 <33526565+laoxiao789@users.noreply.github.com>
2025-06-17 20:37:12 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    // 在构建过程中忽略ESLint错误
-    ignoreDuringBuilds: true,
-  },
-  /* config options here */
+  output: 'export',  
+  trailingSlash: true,  
+  images: {  
+    unoptimized: true  
+  }
 };
 
 module.exports = nextConfig; 
